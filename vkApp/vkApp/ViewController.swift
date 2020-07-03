4d1aa5c4064830626e8926a798b985798c81cc79
kostinem <kostinem@gmail.com>
2020-07-03 21:59:31 +0300

    HomeWork 1

---
+++ vkApp/vkApp/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  vkApp
+//
+//  Created by Бектемир Закиров on 03.07.2020.
+//  Copyright © 2020 Бектемир Закиров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            scrollView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func keyboardWillShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0)
+        
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
+    }
+        
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+            
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+   
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
+    }
+
+
+
+
